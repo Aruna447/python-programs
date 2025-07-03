86298647dca245a88e4f002d42d02f003bdcd820
Aruna447 <arunabodapati21@gmail.com>
2025-07-03 13:54:56 +0530

    Add files via upload

---
+++ listmethods.py
@@ -0,0 +1,51 @@
+"""slicing in used to retrieve the elements from one particular range to another particular range 
+the synatx: s^3(start:stop:skip)
+"""
+# mylist =  [1,2,13,15,16,17,18,19]
+# print(mylist)
+# print(mylist[0:6:2])
+
+
+# mylist1=[2,5,6,7,8,9,10]
+# print(mylist1[0:7:3])
+
+
+# mylist3=[45,46,78,90,56,34,78,90,34,65,89]
+# print(mylist3[0:8:4])
+
+
+# mylist4=[2,5,6,7,8,9,10]
+# print(mylist4[2:6:2])
+
+
+
+
+"""  list is a collection/group of items/values/elements they can be enclosed within the square brackets[]
+separated by (,) list methods:append/extend/count/mutability/index/remove /pop/insert
+syntax: listname.methodname()
+"""
+# #append:this method is used to add the elements at the end
+# list1=["Edinburgh","scotland","london"]
+# print(list1)
+# list1.append("bhrihingam")
+# print(list1)
+# #extended:this method is used to add the elements at the end but 
+# # as a sublist
+# list1.extend(("paris","Maldives","Bali",))
+# print(list1)
+# #count: This method counts the number of repested elements in a list 
+# flowers=["Rose","lilly","Lotus","jasmine","Lotus","Hibiscus"]
+# flowers.count("Lotus")
+# print(flowers)
+
+
+"""Note:It will take atmost only one arugument
+"""
+#insert--it just inserts the elements into the list
+#using the index
+mylist7=[22,33,44,77,88]
+print(mylist7)
+mylist7.insert(1,"hello")
+print(mylist7)
+
+
